3e67123cf7af7dd2ca14b0492dab9dea016b5c4c
30Dennis <otienodennis69@gmail.com>
2023-08-29 09:44:50 +0300

    Sign up activity

--- app/src/main/java/com/example/megaapp/SignupActivity.kt
+++ app/src/main/java/com/example/megaapp/SignupActivity.kt
@@ -3,44 +3,73 @@ package com.example.megaapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.megaapp.ui.theme.ui.theme.MegaAppTheme
 
 class SignupActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MegaAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting2("Android")
-                }
-            }
+          outerLayout()
         }
     }
 }
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview2() {
-    MegaAppTheme {
-        Greeting2("Android")
+fun outerLayout(){
+    var username by remember {
+       mutableStateOf("")
     }
+    var password by remember {
+        mutableStateOf("")
+    }
+    Column( modifier = Modifier
+        .background(color = Color.Black)
+        .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Column(
+            modifier = Modifier
+                .background(color = Color.Cyan)
+                .padding(all = 20.dp),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(text = "Sign Up", fontFamily = FontFamily.Cursive, fontSize = 60.sp)
+            OutlinedTextField(value = username, onValueChange ={username})
+            Spacer(modifier = Modifier.height(22.dp))
+            OutlinedTextField(value = password, onValueChange ={password} )
+
+        }
+    }
+
 }
